26c2287cd899b296423e6aa82dbc1f337b0b2cde
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2023-12-19 00:26:58 +0200

    add image delelion from s3

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,15 @@
 class Image < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  after_destroy_commit :delete_image_from_s3
+
+  private
+
+  def delete_image_from_s3
+    return unless persisted? # Check if the record exists
+
+    photo.purge if photo.attached?
+    # Delete from S3 and perform any other necessary actions
+  end
 end
