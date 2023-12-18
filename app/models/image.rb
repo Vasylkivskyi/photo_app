8d5b4452786eeeb8642a80a124b8010741c09efc
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2023-12-19 00:33:52 +0200

    add image validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  validates :photo, presence: true
+  validate :image_attachment_type
 
   after_destroy_commit :delete_image_from_s3
 
@@ -11,5 +13,12 @@ class Image < ApplicationRecord
 
     photo.purge if photo.attached?
     # Delete from S3 and perform any other necessary actions
+  end
+
+  def image_attachment_type
+    if photo.attached? && !photo.content_type.start_with?('image/')
+      errors.add(:photo, 'must be an image file')
+      photo.purge if photo.persisted? # Remove the attachment
+    end
   end
 end
